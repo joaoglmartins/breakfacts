c81cf881fd915cc8d1a90c26a0cb40d9e92a9a5d
João Martins <joaogasparlopesmartins@gmail.com>
2025-10-29 11:01:37 +0000

    feat: backend initial commit

---
+++ backend/src/main/java/com/joaoglmartins/breakfactsapi/BreakfactsapiApplication.java
@@ -0,0 +1,14 @@
+package com.joaoglmartins.breakfactsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakfactsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakfactsapiApplication.class, args);
+		System.out.println("LIVE");
+	}
+
+}
---
+++ backend/src/test/java/com/joaoglmartins/breakfactsapi/BreakfactsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoglmartins.breakfactsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakfactsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
